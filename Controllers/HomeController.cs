dbc519d2d5f0a17bcda2e9c038421f06935b76d0
Justin J. Miller <justin@jjmiller.org>
2022-01-25 19:20:06 -0700

    mock up connection to sid service

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Icon.Communication.Sid;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using state_icon_web.Models;
+
+namespace state_icon_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISidClient sidClient;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, ISidClient sidClient)
+        {
+            _logger = logger;
+            this.sidClient = sidClient;
+            sidClient.JwtToken = "123";
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string ssn)
+        {
+            var viewModel = new Icon.Communication.Sid.FindSidViewModel
+            {
+                StateCode = "UT",
+                Ssn = ssn,
+                RequestingUserId = "1234",
+                TerminalId = "1235"
+            };
+
+            var response = sidClient.FindSsnAsync(viewModel);
+
+            ViewData["message"] = response.Result.ToJson();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace state_icon_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
